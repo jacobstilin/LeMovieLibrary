124606999b551f42a627a944744a1cdd309674d4
Jacob Stilin <stilinj@gmail.com>
2019-11-05 12:12:44 -0600

    Added get to front end, fixed multiple seed issues

--- Client/app.js
+++ Client/app.js
@@ -46,10 +46,50 @@
             error: function( jqXhr, textStatus, errorThrown ){
                 console.log( errorThrown );
             }
-        });
-
+        }).then(function(data) {
+            $('tbody').html('');
+            $.each(data, function(index, value){
+                $('tbody').append(
+                    "<tr>" +
+                    "<td>" + value.Title + "</td>" +
+                    "<td>" + value.Genre + "</td>" +
+                    "<td>" + value.Director + "</td>" +
+                    "</tr>"
+                );
+            });
+        }
+        )
         e.preventDefault();
     }
 
     $('#my-form2').submit( processForm2 );
 })(jQuery);
+
+
+// (function getAllMovies(){
+//     $(document).ready(function() {
+//         $.ajax({
+//             type: 'get',
+//             url: 'https://localhost:44352/api/movie',
+//             dataType: 'json',
+//             success: function(){
+//                 $('.Movies').html('');
+//             }
+//         })
+//         .then(function(data) {
+//             $.each(data, function(index, value){
+//                 $('myTable').append(
+//                     "<tr>" +
+//                     "<td>" + value.Title + "</td>" +
+//                     "<td>" + value.Genre + "</td>" +
+//                     "<td>" + value.Director + "</td>" +
+//                     "</tr>"
+//                 );
+//             });
+//         }
+//         )
+//     }
+//     )
+    
+// })(jQuery);
+
